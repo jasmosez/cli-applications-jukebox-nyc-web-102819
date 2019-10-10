8ed62d67f0e1efc825cd76e554b29bf7dea1021e
James Schaffer <jas.schaffer@gmail.com>
2019-10-10 22:23:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,7 +17,7 @@ def validate_selection(song_request, songs)
 
   song_number = song_request.to_i
   #if song_request is not an integer, song_number will be zero
-
+  binding.pry
   if song_number > 0 && song_number <= songs.length
     return songs[song_number - 1]
   #song_request == valid song title
@@ -36,6 +36,7 @@ def play(songs)
   until valid == true
     play_prompt
     song_request = gets.strip
+    binding.pry
     jukebox_response = validate_selection(song_request, songs)
     if jukebox_response
       valid = true
