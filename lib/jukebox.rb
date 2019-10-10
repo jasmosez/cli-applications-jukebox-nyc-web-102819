2568078b6602e516a8435fe32c56333bd580d970
James Schaffer <jas.schaffer@gmail.com>
2019-10-10 22:08:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,8 +17,7 @@ def validate_selection(song_request, songs)
 
   song_number = song_request.to_i
   #if song_request is not an integer, song_number will be zero
-  binding.pry
-  
+
   if song_number > 0 && song_number <= songs.length
     return songs[song_number - 1]
   #song_request == valid song title
